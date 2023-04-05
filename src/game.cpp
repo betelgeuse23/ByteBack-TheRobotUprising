f220447f6c7fb48d32a85d636b18f9d8bf74ca1f
Anastasia Melnikova <ann.melnikova23@gmail.com>
2023-04-05 12:36:06 +0300

    work with window

---
+++ src/game.cpp
@@ -0,0 +1,33 @@
+﻿// game.cpp: определяет точку входа для приложения.
+//
+
+#include "game.h"
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+	// Создаем окно
+	sf::RenderWindow window(sf::VideoMode(600, 600), "game", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
+	window.setFramerateLimit(60);
+
+	sf::Event event;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) window.close();
+			if (event.type == sf::Event::KeyPressed)
+			{
+				// Закрываем окно
+				if (event.key.code == sf::Keyboard::Escape) window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}
