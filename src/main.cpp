5529830e11358c988577327ad75d53f0f84e979c
Anastasia Melnikova <ann.melnikova23@gmail.com>
2023-04-05 20:53:25 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 ﻿#include "main.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(512, 256), "Tilemap");
+    sf::RenderWindow window(sf::VideoMode(512, 256), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
+    window.setFramerateLimit(60);
 
     const int level[] = {
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        39, 39, 39, 39, 39, 39, 33, 34, 35, 36, 37, 38, 39, 0, 1, 1,
         0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
         1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
         0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
@@ -15,17 +16,23 @@ int main() {
     };
 
     TileMap map;
-    if (!map.load("images/tileset.png", sf::Vector2u(32, 32), level, 16, 8))
+    if (!map.load("images/test.png", sf::Vector2u(32, 32), level, 16, 8))
         return -1;
 
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                // Закрываем окно
+                if (event.key.code == sf::Keyboard::Escape) window.close();
+            }
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
         window.draw(map);
         window.display();
     }
