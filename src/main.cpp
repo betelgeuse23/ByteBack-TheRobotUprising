f8d850cbeca933a7e82249b95827ba441282a240
Anastasia Melnikova <ann.melnikova23@gmail.com>
2023-04-06 16:18:03 +0300

    animation and movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 ﻿#include "main.h"
 
+double frame = 0;
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(512, 512), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(512, 512), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar);
     window.setFramerateLimit(60);
+    sf::Clock clock;
 
     // персонаж
     sf::Image image;
-    image.loadFromFile("images/players.png");
+    image.loadFromFile("images/robot2.png");
     sf::Texture texture;
     texture.loadFromImage(image);
     sf::Sprite player;
     player.setTexture(texture);
-    player.setTextureRect(sf::IntRect(0, 0, 32, 32));
+    player.setTextureRect(sf::IntRect(0, 0, 31, 31));
     player.setPosition(0, 0);
 
 
@@ -43,6 +46,10 @@ int main() {
     // работа с окном
     while (window.isOpen())
     {
+        float time = clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time = time / 2800;
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -53,6 +60,23 @@ int main() {
                 if (event.key.code == sf::Keyboard::Escape) window.close();
             }
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            player.move(-1.0 * time, 0);
+
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            player.move(1.0 * time, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            player.move(0, -1.0 * time);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            player.move(0, 1.0 * time);
+        }
+        frame += 0.005 * time;
+        if (frame > 4) frame -= 4;
+        player.setTextureRect(sf::IntRect(32 * int(frame), 0, 31, 31));
 
         window.clear(sf::Color::Black);
         window.draw(map);
