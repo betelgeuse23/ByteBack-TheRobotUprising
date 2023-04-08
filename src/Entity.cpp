c049709c9fb8783b3c5cb3483c345004171035b0
NigthLier <102072748+NigthLier@users.noreply.github.com>
2023-04-09 02:22:22 +0300

    entity

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,84 +1,78 @@
 #include "Entity.h"
 
-void log_i(std::string s) {
-	std::cout << s << std::endl;
-}
-
-Entity::Entity(std::string texture, sf::Vector2f position) {
+Entity::Entity(std::string texture, sf::Vector2i position) {
 	this->position = position;
 	this->texture.loadFromFile(texture);
 	this->sprite.setTexture(this->texture);
 	this->sprite.setTextureRect(sf::IntRect(0, 0, 31, 31));
-	this->sprite.setPosition(position);
+	this->sprite.setPosition(sf::Vector2f(position * cell));
 }
 
-void Entity::draw(sf::RenderWindow& window) { window.draw(sprite); }
+void Entity::initPatfind(const int* level, sf::Vector2i size, std::map<int, int> costs) {
+	pf = PathFinder(level, size, costs);
+}
 
-void Entity::animate(int ch, int sec) {
-	this->sprite.setTextureRect(sf::IntRect(32 * (int(ch * sec / cell) % sec), 0, 31, 31));
+void Entity::animate(State st, int fr, int sec) {
+	this->sprite.setTextureRect(sf::IntRect(32 * ((fr * sec / cell) % sec), 32 * st, 31, 31));
+}
+
+void Entity::move(sf::Vector2i dest) {
+	if(pf.isInit()) move(pf.pathfind(position, dest));
 }
 
 void Entity::move(Direction dir) {
-	if (this->state == Still) {
-		this->clock.restart();
-		this->direction = dir;
-		this->state = Moving;
+	if (state == Still) {
+		clock.restart();
+		direction = dir;
+		state = Moving;
 	}
-}
+	else if (state == Moving && direction) {
+		if (!(pf.isInit()) || (pf.isInit() && pf.isAccesible(direction, position))) {
+			sf::Vector2i t_pos = position * cell;
+			int time = clock.getElapsedTime().asMilliseconds();
+			int change = std::min(int(time * speed), cell);
 
-void Entity::move() {
-	if (this->direction) {
-		sf::Vector2f t_pos = this->position;
-		int time = this->clock.getElapsedTime().asMilliseconds();
-		int change = std::min(int(time * speed), cell);
-		
-		if (this->direction == 1)
-			t_pos.y -= change;
-		else if (this->direction == 2)
-			t_pos.y += change;
-		else if (this->direction == 3)
-			t_pos.x -= change;
-		else if (this->direction == 4)
-			t_pos.x += change;
+			t_pos += Utils::makeDir(direction) * change;
 
-		this->animate(change, 4);
-		this->sprite.setPosition(t_pos);
+			animate(Moving, change, 4);
+			sprite.setPosition(sf::Vector2f(t_pos));
 
-		if (change == cell) {
-			this->position = t_pos;
-			this->direction = None;
-			this->state = Still;
+			if (change == cell) {
+				position += Utils::makeDir(direction);
+				direction = None;
+				state = Still;
+			}
 		}
-	}
-	else this->state = Still;
+		
+	} else state = Still;
 }
 
-void Entity::info() {
-	log_i(std::to_string(this->state) + " " + std::to_string(this->direction));
+
+
+PathFinder::PathFinder(const int* level, sf::Vector2i size, std::map<int, int> costs)
+	: size(size), accesible(std::vector<std::vector<int>>(size.x, std::vector<int>(size.y))), level(level), costs(costs), init(true) {
+	makeCosts();
 }
 
-Direction pathfind(const int* level, sf::Vector2f size, sf::Vector2f pos, sf::Vector2f dest) {
+Direction PathFinder::pathfind(sf::Vector2i pos, sf::Vector2i dest) {
+	if(!(init) || dest.x < 0 || dest.y < 0 || dest.x >= size.x || dest.y >= size.y || pos.x < 0 || pos.y < 0 || pos.x >= size.x || pos.y >= size.y) return Direction::None;
+
+	makeCosts();
+
 	struct Node {
-		sf::Vector2f v;
+		sf::Vector2i v;
 		int c;
 		int f;
-		Node::Node(sf::Vector2f v, int c, int f) : v(v), c(c), f(f) {};
+		Node::Node(sf::Vector2i v, int c, int f) : v(v), c(c), f(f) {};
 		inline bool operator==(const Node& oth) { return (this->v == oth.v) && (this->c == oth.c); }
 	};
-	
-	auto compare = [](Node n1, Node n2) {return n1.c > n2.c; };
-	auto distance = [](sf::Vector2f p1, sf::Vector2f p2) {return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2)); };
+
+	auto compare = [](const Node n1, const Node n2) {return n1.f > n2.f; };
+	auto distance = [](const sf::Vector2i p1, const sf::Vector2i p2) {return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2)); };
 
 	std::vector<Node> visited, opened;
-	std::vector<std::vector<int>> costs(size.x, std::vector<int>(size.y, size.x* size.y + 10));
-	for (int i = 0; i < size.x; i++)
-		for (int j = 0; j < size.y; j++)
-			if (level[i  + j * int(size.x)] != 0)
-				costs[i][j] = -1;
+	std::vector<std::vector<int>> costs(size.x, std::vector<int>(size.y, size.x * size.y + 10));
 
-	if(	dest.x < 0 || dest.y < 0 || dest.x >= size.x || dest.y >= size.y ||
-		pos.x < 0 || pos.y < 0 || pos.x >= size.x || pos.y >= size.y ||
-		costs[dest.x][dest.y] == -1) return Direction::None;
 	costs[dest.x][dest.y] = 0;
 	opened.push_back(Node(dest, 0, distance(dest, pos)));
 
@@ -88,10 +82,10 @@ Direction pathfind(const int* level, sf::Vector2f size, sf::Vector2f pos, sf::Ve
 		opened.pop_back();
 
 		std::vector<Node> neigbors;
-		if (curr.v.y > 0 && costs[curr.v.x][curr.v.y - 1] != -1) neigbors.push_back(Node(curr.v - sf::Vector2f(0, 1), costs[curr.v.x][curr.v.y - 1], costs[curr.v.x][curr.v.y - 1] + distance(curr.v - sf::Vector2f(0, 1), pos)));
-		if (curr.v.x > 0 && costs[curr.v.x - 1][curr.v.y] != -1) neigbors.push_back(Node(curr.v - sf::Vector2f(1, 0), costs[curr.v.x - 1][curr.v.y], costs[curr.v.x - 1][curr.v.y] + distance(curr.v - sf::Vector2f(1, 0), pos)));
-		if (curr.v.y < size.y - 1 && costs[curr.v.x][curr.v.y + 1] != -1) neigbors.push_back(Node(curr.v + sf::Vector2f(0, 1), costs[curr.v.x][curr.v.y + 1], costs[curr.v.x][curr.v.y + 1] + distance(curr.v + sf::Vector2f(0, 1), pos)));
-		if (curr.v.x < size.x - 1 && costs[curr.v.x + 1][curr.v.y] != -1) neigbors.push_back(Node(curr.v + sf::Vector2f(1, 0), costs[curr.v.x + 1][curr.v.y], costs[curr.v.x + 1][curr.v.y] + distance(curr.v + sf::Vector2f(1, 0), pos)));
+		if (curr.v.y > 0 && accesible[curr.v.x][curr.v.y - 1]) neigbors.push_back(Node(curr.v + Utils::makeDir(Up), costs[curr.v.x][curr.v.y - 1], costs[curr.v.x][curr.v.y - 1] + distance(curr.v + Utils::makeDir(Up), pos)));
+		if (curr.v.x > 0 && accesible[curr.v.x - 1][curr.v.y]) neigbors.push_back(Node(curr.v + Utils::makeDir(Left), costs[curr.v.x - 1][curr.v.y], costs[curr.v.x - 1][curr.v.y] + distance(curr.v + Utils::makeDir(Left), pos)));
+		if (curr.v.y < size.y - 1 && accesible[curr.v.x][curr.v.y + 1]) neigbors.push_back(Node(curr.v + Utils::makeDir(Down), costs[curr.v.x][curr.v.y + 1], costs[curr.v.x][curr.v.y + 1] + distance(curr.v + Utils::makeDir(Down), pos)));
+		if (curr.v.x < size.x - 1 && accesible[curr.v.x + 1][curr.v.y]) neigbors.push_back(Node(curr.v + Utils::makeDir(Right), costs[curr.v.x + 1][curr.v.y], costs[curr.v.x + 1][curr.v.y] + distance(curr.v + Utils::makeDir(Right), pos)));
 		std::sort(neigbors.begin(), neigbors.end(), compare);
 
 		if (curr.v == pos) {
@@ -104,16 +98,16 @@ Direction pathfind(const int* level, sf::Vector2f size, sf::Vector2f pos, sf::Ve
 			}
 			return Direction::None;
 		}
-		
+
 		visited.push_back(curr);
 
 		for (auto nb : neigbors) {
 			if (std::find(visited.begin(), visited.end(), nb) != visited.end()) continue;
 
 			if (nb.c == size.x * size.y + 10 || nb.c > curr.c + 1) {
-				costs[nb.v.x][nb.v.y] = curr.c + 1;
-				if (std::find(opened.begin(), opened.end(), nb) == opened.end()) opened.push_back(Node(nb.v, curr.c + 1, curr.c + 1 + distance(nb.v, pos)));
-				else *std::find(opened.begin(), opened.end(), nb) = Node(nb.v, curr.c + 1, curr.c + 1 + distance(nb.v, pos));
+				costs[nb.v.x][nb.v.y] = curr.c + accesible[nb.v.x][nb.v.y];
+				if (std::find(opened.begin(), opened.end(), nb) == opened.end()) opened.push_back(Node(nb.v, costs[nb.v.x][nb.v.y], costs[nb.v.x][nb.v.y] + distance(nb.v, pos)));
+				else *std::find(opened.begin(), opened.end(), nb) = Node(nb.v, costs[nb.v.x][nb.v.y], costs[nb.v.x][nb.v.y] + distance(nb.v, pos));
 			}
 		}
 	}
@@ -121,8 +115,43 @@ Direction pathfind(const int* level, sf::Vector2f size, sf::Vector2f pos, sf::Ve
 	return Direction::None;
 }
 
-sf::Vector2f gPos(sf::Vector2f pos){
-	pos.x = roundf(pos.x / cell);
-	pos.y = roundf(pos.y / cell);
-	return pos;
+void PathFinder::info() {
+	if (accesible.empty()) std::cout << "Empty" << std::endl;
+	else Utils::vecCout(size, accesible);
+}
+
+bool PathFinder::isAccesible(const Direction dir, const sf::Vector2i pos) {
+	sf::Vector2i posN = pos + Utils::makeDir(dir);
+	if (!(init) || posN.x < 0 || posN.y < 0 || posN.x >= size.x || posN.y >= size.y || !(accesible[posN.x][posN.y])) return false;
+	return true;
+}
+
+void PathFinder::makeCosts() {
+	for (int i = 0, t = 0; i < size.x; i++) for (int j = 0; j < size.y; j++) accesible[j][i] = costs[level[t++]];
+}
+
+
+
+sf::Vector2i Utils::makeDir(Direction dir) {
+	switch (dir) {
+	case Direction::Up: return sf::Vector2i(0, -1);
+	case Direction::Down: return sf::Vector2i(0, 1);
+	case Direction::Left: return sf::Vector2i(-1, 0);
+	case Direction::Right: return sf::Vector2i(1, 0);
+	default: return sf::Vector2i(0, 0);
+	}
+}
+
+sf::Vector2i Utils::gPos(sf::Vector2f pos) {
+	return sf::Vector2i(roundf(pos.x / cell), roundf(pos.y / cell));
+}
+
+void Utils::vecCout(sf::Vector2i size, std::vector<std::vector<int>> vec) {
+	for (int i = 0; i < size.x; i++) {
+		for (int j = 0; j < size.y; j++) {
+			std::cout.width(3);
+			std::cout << vec[i][j] << " ";
+		}
+		std::cout << std::endl;
+	} std::cout << std::endl;
 }
--- src/Entity.h
+++ src/Entity.h
@@ -3,41 +3,66 @@
 #define cell 32
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
 #include <vector>
-#include <queue>
-#include <array>
-#include <string>
-
-void log_i(std::string s);
+#include <iostream>
 
 enum Direction {
     None, Up, Down, Left, Right
 };
 
 enum State {
-    Still, Moving, Shooting, Dead
+    Moving, Still, Shooting, Dead
+};
+
+
+class PathFinder {
+public:
+    PathFinder() {};
+    PathFinder(const int*, sf::Vector2i, std::map<int, int>);
+    Direction pathfind(sf::Vector2i, sf::Vector2i);
+    std::vector<std::vector<int>>* getAcceses() { return &accesible; };
+    void info();
+    bool isInit() { return init; };
+    bool isAccesible(const Direction, const sf::Vector2i);
+private:
+    void makeCosts();
+    const int* level;
+    std::vector<std::vector<int>> accesible;
+    std::map<int, int> costs;
+    sf::Vector2i size;
+    bool init = false;
 };
 
 class Entity {
 public:
-    Entity(std::string texture, sf::Vector2f position);
-    void draw(sf::RenderWindow& window);
-    void animate(int ch, int sec);
-    void move(Direction dir);
-    void move();
-    sf::Vector2f pos() { return position; };
-    void info();
+    Entity(std::string, sf::Vector2i);
+    inline void draw(sf::RenderWindow& window) { window.draw(sprite); };
+    void initPatfind(const int*, sf::Vector2i, std::map<int, int>);
+
+    void move(sf::Vector2i);
+    void move(Direction);
+
+    sf::Vector2i getPosition() { return position; };
+    State getState() { return state; };
+
+    void setSpeed(float speed) { this->speed = speed; };
+    void setState(State state) { this->state = state; };
 private:
+    PathFinder pf;
     sf::Clock clock;
     sf::Sprite sprite;
     sf::Texture texture;
-    sf::Vector2f position;
+    sf::Vector2i position;
     Direction direction = None;
     State state = Still;
     float speed = 0.1;
+
+    void animate(State, int, int);
 };
 
-Direction pathfind(const int* level, sf::Vector2f size, sf::Vector2f pos, sf::Vector2f dest);
-
-sf::Vector2f gPos(sf::Vector2f pos); 
+class Utils {
+public:
+    static sf::Vector2i makeDir(Direction);
+    static sf::Vector2i gPos(sf::Vector2f);
+    static void vecCout(sf::Vector2i, std::vector<std::vector<int>>);
+};
