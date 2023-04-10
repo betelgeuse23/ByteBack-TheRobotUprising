332f5dce48c9d2b8d28f04c6ef7e4583fc40e3af
Anastasia Melnikova <ann.melnikova23@gmail.com>
2023-04-10 07:53:38 +0300

    developing game menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 ﻿#include "main.h"
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode((WIDTH + 8) * 32, WIDTH * 32), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar);
+void startGame() {
+    sf::RenderWindow window(sf::VideoMode((WIDTH + 6) * 32, WIDTH * 32), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar);
     window.setFramerateLimit(60);
+    window.setMouseCursorVisible(false);
     sf::Clock clock;
     std::vector<Bullet> bullets;
 
@@ -60,8 +61,7 @@ int main() {
     };
 
     TileMap map;
-    if (!map.load("images/tileset.png", sf::Vector2u(32, 32), level, WIDTH, WIDTH))
-        return -1;
+    map.load("images/tileset.png", sf::Vector2u(32, 32), level, WIDTH, WIDTH);
 
     bool isMoving = false;
     char dir = 'n';
@@ -123,28 +123,31 @@ int main() {
             else isShooting = false;
         }
 
-        if (isMoving && dir == 'n' && delay/1000 > 0.1) isMoving = false;
+        if (isMoving && dir == 'n' && delay / 1000 > 0.1) isMoving = false;
 
-        if (isMoving && move < 32 && dir != 'n') {
+        if (isMoving&& move < 32 && dir != 'n') {
             move += 0.2 * time;
             float k = move > 32 ? move - 32 : 0;
             if (dir == 'u') {
                 player.move(0, -0.2 * time + k);
-            } else if (dir == 'd') {
+            }
+            else if (dir == 'd') {
                 player.move(0, 0.2 * time - k);
-            } else if (dir == 'r') {
+            }
+            else if (dir == 'r') {
                 player.move(0.2 * time - k, 0);
-            } else if (dir == 'l') {
+            }
+            else if (dir == 'l') {
                 player.move(-0.2 * time + k, 0);
             }
-            player.setTextureRect(sf::IntRect(32 * (int(move/16) % 2), 0, 31, 31));
+            player.setTextureRect(sf::IntRect(32 * (int(move / 16) % 2), 0, 31, 31));
         }
         else {
             move = 0;
             dir = 'n';
             player.setTextureRect(sf::IntRect(0, 0, 31, 31));
         }
-        
+
         window.clear(sf::Color::Black);
         window.draw(map);
         window.draw(healthText);
@@ -158,10 +161,9 @@ int main() {
             //}
             if (bullet.checkCollisionsWithObstacle(level)) {
                 bullets.erase(std::remove(bullets.begin(), bullets.end(), bullet), bullets.end());
-                if (!map.load("images/tileset.png", sf::Vector2u(32, 32), level, WIDTH, WIDTH))
-                    return -1;
+                map.load("images/tileset.png", sf::Vector2u(32, 32), level, WIDTH, WIDTH);
             }
-            else if (bullet.getPosition().y < 0 && bullet.getDirection() == 'u' || 
+            else if (bullet.getPosition().y < 0 && bullet.getDirection() == 'u' ||
                 bullet.getPosition().y > 672 && (bullet.getDirection() == 'd' || bullet.getDirection() == 'n') ||
                 bullet.getPosition().x < 0 && bullet.getDirection() == 'l' ||
                 bullet.getPosition().x > 672 && bullet.getDirection() == 'r') {
@@ -176,6 +178,59 @@ int main() {
         window.draw(player);
         window.display();
     }
+}
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode((WIDTH + 6) * 32, WIDTH * 32), "Metal-Menace", sf::Style::Close | sf::Style::Titlebar);
+    window.setFramerateLimit(60);
+    sf::Clock clock;
+
+    TileMap menuMap;
+    int map[729] = { 0 };
+    menuMap.load("images/tileset.png", sf::Vector2u(32, 32), map, WIDTH + 6, WIDTH);
+
+    // текст
+    sf::Font fontPixel;
+    fontPixel.loadFromFile("fonts/pixel.ttf");
+
+    sf::Font fontArial;
+    fontArial.loadFromFile("fonts/arial.ttf");
+
+    sf::Text Titul("Metal Menace", fontPixel, 50);
+    Titul.setLetterSpacing(2);
+    Titul.setFillColor(sf::Color::White);
+    Titul.setPosition(10, 10);
+
+
+    std::string name_menu[]{ "Start", "Continue", "Configure", "About", "Exit" };
+    /*game::GameMenu mymenu(window, 950, 350, 4, name_menu, 100, 120);
+    mymenu.setColorTextMenu(Color(237, 147, 0), Color::Red, Color::Black);
+    mymenu.AlignMenu(2);*/
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window.close();
+            /*if (event.key.code == sf::Keyboard::Up) { mymenu.MoveUp(); }
+            if (event.key.code == sf::Keyboard::Down) { mymenu.MoveDown(); }
+            if (event.key.code == sf::Keyboard::Return)
+            {
+                switch (mymenu.getSelectedMenuNumber())
+                {
+                case 0:startGame();   break;
+                case 1:Options();     break;
+                case 2:About_Game();  break;
+                case 3:window.close(); break;
+
+                }
+            }*/
+            window.clear(sf::Color::Black);
+            window.draw(menuMap);
+            window.draw(Titul);
+        }
+        window.display();
+    }
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -3,6 +3,8 @@
 #define WIDTH 21
 
 #include <iostream>
+#include <string>
 #include "TileMap.h"
 #include "bullet.h"
 #include "Entity.h"
+//#include "menu.h"
